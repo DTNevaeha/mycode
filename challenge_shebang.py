b80efc57b92a11ff109af3c4043d49738dc72254
DTNevaeha <DTNevaeha@users.noreply.github.com>
2022-09-19 22:26:13 +0000

    Finished Shebang Challenge

---
+++ challenge_shebang.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+  
+    # collect string input from the user
+    name = input("Enter you name!")
+
+    day = input("Enter the day of the week!")
+
+    #print user input to look like this "Hello, <name>! Happy <day of the week>!"
+    print("Hello, " + name + "! Happy " + day + "!" )
+main()               
